11404b83822afbcd3ab89a09124f9389481cde67
Mikhail Davydov <azazel.private@gmail.com>
2015-06-24 00:33:44 +0200

    Serializers refactoring

--- example/index.js
+++ example/index.js
@@ -1,11 +1,11 @@
 import './index.css';
-import {TuiElement, render, compressBox} from '..';
+import {TuiElement, render} from '..';
 
 requestAnimationFrame(function () {
     var element = new TuiElement(document.querySelector('.tui-dom'), {
         scale: [7.8, 13]
     });
-    var serializedElement = compressBox(element.toArray());
+    var serializedElement = element.toCompressedArray();
 
     document.querySelector('.tui-cli').innerHTML = render.html(serializedElement);
     if (/PhantomJS/.test(window.navigator.userAgent)) {
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 export {default as TuiElement} from './lib/tui-element';
 export {default as render} from './lib/renders';
-export {default as compressBox} from './lib/compress-box';
 export {addSerializer} from './lib/serializers';
